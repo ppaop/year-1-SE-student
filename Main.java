5f6dbf0a35e54fcff4c0a7ef26d7f14f89d2e864
ppaop <81806964+ppaop@users.noreply.github.com>
2024-10-02 00:56:30 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static int gcd(int a,int b){
+        int x=Math.max(a,b);
+        int y=Math.min(a,b);
+        while (true){
+            int n= x%y;
+            if (n!=0){
+                x=y;y=n;
+            }else return y;
+        }
+    }
+    public static int lcm(int a,int b){
+        return a*b/(gcd(a,b));
+    }
+    public static void main(String[] args) {
+        Scanner kb =new Scanner(System.in);
+        int a = kb.nextInt();
+        int b= kb.nextInt();
+        System.out.println(gcd(a,b));
+        System.out.println(lcm(a,b));
+    }
+}
+
+
